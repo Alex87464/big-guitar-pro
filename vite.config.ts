d5428fed7749e0cc87760fbd029d7723b1861527
Alex Oliva <alexoliva87464@gmail.com>
2024-06-04 09:03:44 -0300

    chore: Add @coderline/alphatab dependency to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { alphaTab } from '@coderline/alphatab/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), alphaTab()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
