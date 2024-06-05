89262407f8c25d43fea2b98b572546fb6981e060
Alex Oliva <alexoliva87464@gmail.com>
2024-06-05 06:41:06 -0300

    Update PlayerNav component to remove unused imports and improve track selection logic

--- src/components/PlayerNav.tsx
+++ src/components/PlayerNav.tsx
@@ -2,12 +2,14 @@ import { Button } from '@/components/ui/button';
 import { Slider } from '@/components/ui/slider';
 import { Label } from './ui/label';
 import {
+  CircleHelp,
   Headphones,
   PlayCircle,
   Repeat,
   Repeat1,
   SkipBack,
   SkipForward,
+  Speaker,
   TimerOff,
   TimerReset,
   Volume1,
@@ -25,19 +27,20 @@ interface PlayerNavProps {
   api: AlphaTabApi | undefined;
 }
 export default function PlayerNav({ api }: PlayerNavProps) {
-  const tracks = api?.score?.tracks;
+  const [tracks, setTracks] = useState([] as any[]);
+
+  const [actualTrack, setActualTrack] = useState<any>(api?.tracks[0]); // Track the actual rendered track
+
   const [isPlaying, setIsPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
   const [isRepeat, setIsRepeat] = useState(false);
   const [isMetronome, setIsMetronome] = useState(false);
   const [isCountDownEnabled, setIsCountDownEnabled] = useState(false);
   const [tempo, setTempo] = useState(100);
-
   const handlePlayPause = () => {
     setIsPlaying(!isPlaying);
     api?.playPause();
   };
-
   const handleRepeat = () => {
     setIsRepeat((prevRepeat) => !prevRepeat);
     api!.isLooping = !isRepeat;
@@ -57,6 +60,11 @@ export default function PlayerNav({ api }: PlayerNavProps) {
     setTempo(value);
     api!.playbackSpeed = value / 100;
   };
+
+  useEffect(() => {
+    setTracks(api?.score?.tracks || []);
+    setActualTrack(api?.tracks[0]);
+  }, [tracks]);
 
   return (
     <div className='fixed bottom-0 z-10 w-full flex justify-center pb-3'>
@@ -115,10 +123,30 @@ export default function PlayerNav({ api }: PlayerNavProps) {
           </Toggle>
           <Button
             variant='outline'
+            size='icon'
             className='text-black'
-            onClick={() => console.log('Check actual track')}
+            // onClick={() => console.log(api?.score?.tracks[actualTrack].name)} // This prints the actual track inside score
+            onClick={() => console.log(api?.tracks)} // This prints the actual rendered track
           >
-            Check actual track
+            <CircleHelp />
+          </Button>
+          <Button
+            variant='outline'
+            className='text-black w-10'
+            size='icon'
+            // onClick={() => console.log(api?.score?.tracks[actualTrack].name)} // This prints the actual track inside score
+            onClick={() => api?.changeTrackMute([actualTrack], true)} // This prints the actual rendered track
+          >
+            <VolumeX />
+          </Button>
+          <Button
+            variant='outline'
+            className='text-black'
+            size='icon'
+            // onClick={() => console.log(api?.score?.tracks[actualTrack].name)} // This prints the actual track inside score
+            onClick={() => api?.changeTrackMute([actualTrack], false)} // This prints the actual rendered track
+          >
+            <Speaker />
           </Button>
         </div>
 
@@ -153,13 +181,19 @@ export default function PlayerNav({ api }: PlayerNavProps) {
                 <h4 className='scroll-m-20 text-xl font-semibold tracking-tight border-b'>
                   Instrumentos
                 </h4>
-                {api?.score?.tracks.map((track) => (
+                {tracks?.map((track) => (
                   <div
                     key={track.index}
-                    onClick={() =>
-                      api.renderTracks([api.score!.tracks[track.index]])
-                    }
-                    className='flex gap-5 items-center justify-between border-muted p-2 rounded-lg hover:border-slate-800 border-[1px] cursor-pointer'
+                    onClick={() => {
+                      api!.renderTracks([track]);
+                      setActualTrack(track);
+                    }}
+                    className={clsx(
+                      {
+                        'border-blue-500': actualTrack?.index === track.index,
+                      },
+                      'flex gap-5 items-center justify-between  p-2 rounded-lg hover:border-slate-800 border-[1px] cursor-pointer'
+                    )}
                   >
                     {track.staves[0].isPercussion ? (
                       <GiDrumKit size={32} />
