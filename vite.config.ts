313fffa368f827b09bb56a07e2e55a572d8b4f05
Alex Oliva <alexoliva87464@gmail.com>
2024-10-03 22:21:23 -0300

    feat: Add export configs to support tauri

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,30 @@ import { alphaTab } from '@coderline/alphatab/vite';
 
 export default defineConfig({
   plugins: [react(), alphaTab()],
+  clearScreen: false,
+  server: {
+    strictPort: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  envPrefix: [
+    'VITE_',
+    'TAURI_PLATFORM',
+    'TAURI_ARCH',
+    'TAURI_FAMILY',
+    'TAURI_PLATFORM_VERSION',
+    'TAURI_PLATFORM_TYPE',
+    'TAURI_DEBUG',
+  ],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
 });
